cf021fa210c6aab079893291c5b226a3e2e9a14a
naviailpach <ivan.chaplya13@gmail.com>
2026-02-18 20:27:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,52 @@
 'use strict';
 
 const tree = document.querySelector('.tree');
-const listItems = tree.querySelectorAll('li');
 
-listItems.forEach((list) => {
-  const nestedList = list.querySelector('ul');
-
-  if (nestedList) {
-    const children = Array.from(list.childNodes);
-    const textNode = children.find((child) => {
-      return child.nodeType === 3 && child.textContent.trim() !== '';
-    });
-
-    if (textNode) {
-      const span = document.createElement('span');
-
-      span.textContent = textNode.textContent.trim();
-
-      list.insertBefore(span, nestedList);
-      list.removeChild(textNode);
-    }
-  }
-});
-
-tree.addEventListener('click', (e) => {
-  const span = e.target.closest('span');
-
-  if (!span || !tree.contains(span)) {
+function initTree() {
+  if (!tree) {
     return;
   }
 
-  const nestedList = span.nextElementSibling;
+  const listItems = tree.querySelectorAll('li');
 
-  if (nestedList && nestedList.tagName === 'UL') {
-    nestedList.hidden = !nestedList.hidden;
-  }
-});
+  listItems.forEach((list) => {
+    const nestedList = list.querySelector('ul');
+
+    if (nestedList) {
+      const children = Array.from(list.childNodes);
+      const textNode = children.find((child) => {
+        return (
+          (child.nodeType === 3 && child.textContent.trim() !== '') ||
+          child.nodeType === 1
+        );
+      });
+
+      if (textNode) {
+        const span = document.createElement('span');
+
+        span.classList.add('tree__title');
+
+        span.textContent = textNode.textContent.trim();
+
+        list.insertBefore(span, nestedList);
+        list.removeChild(textNode);
+      }
+    }
+  });
+
+  tree.addEventListener('click', (e) => {
+    const span = e.target.closest('.tree__title');
+
+    if (!span || !tree.contains(span)) {
+      return;
+    }
+
+    const nestedList = span.nextElementSibling;
+
+    if (nestedList && nestedList.tagName === 'UL') {
+      nestedList.hidden = !nestedList.hidden;
+    }
+  });
+}
+
+initTree();
